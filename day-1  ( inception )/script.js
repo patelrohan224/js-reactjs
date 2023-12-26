a4c9c7a0b4cabb84447785474442e9fce3982b81
Patel Rohan <rohan_fw11_289@masai.school>
2023-12-26 18:02:30 +0000

    Inception done

--- day-1  ( inception )/script.js
+++ day-1  ( inception )/script.js
@@ -1,8 +1,43 @@
-const h1 = React.createElement(
-  "h1",
-  { id: "attributes", anything: "anything" },
-  "text"
-);
-console.log("🚀 ~ file: script.js:6 ~ h1:", h1);
+// const h1 = React.createElement(
+//   "h1",
+//   { id: "attributes", anything: "anything" },
+//   "text"
+// );
+// console.log("🚀 ~ file: script.js:6 ~ h1:", h1);
+
+/**
+<div id="parent">
+  <div id="child">
+    <h1>h1</h1>
+    <h2>h2</h2>
+  </div>
+  <div id="child2">
+    <h1>h1</h1>
+    <h2>h2</h2>
+  </div>
+</div>;
+ */
+
+// const div = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", { id: "child" }, "h1")
+//   )
+// );
+
+const div = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "h1"),
+    React.createElement("h2", {}, "h2"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "h1"),
+    React.createElement("h2", {}, "h2"),
+  ]),
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(h1);
+root.render(div); // it will replace this root elements of html to above div
